4a0708468251f028e0c743993994ff94284b375a
Rebecca <rebecca.bartram@hotmail.co.uk>
2024-10-07 15:47:10 +0100

    removing comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-// import Image from "next/image";
-// import styles from "./page.module.css";
 import Navigation from "./components/Navigation/page";
 import Header from './components/Header/page';
 
